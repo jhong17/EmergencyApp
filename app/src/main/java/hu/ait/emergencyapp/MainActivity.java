aeed15b735faaba547c5c0828accbbc0367d9a0f
Sooyoung Kim <sooyoung.kim@edu.chat>
2017-07-02 07:26:28 -0400

    spinner dialog works woooooooooooooooohooo

--- app/src/main/java/hu/ait/emergencyapp/MainActivity.java
+++ app/src/main/java/hu/ait/emergencyapp/MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends AppCompatActivity
 
     private void showSpinnerDialog() {
 
-        //i
+        //
 
         AlertDialog.Builder b = new AlertDialog.Builder(this);
         b.setTitle("Choose a City");
